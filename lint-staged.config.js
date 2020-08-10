8e26554ce48d71c0d835ca86fdc50f89c1234590
姚绍华 <eurallyao@163.com>
2020-08-10 22:33:52 +0800

    再次测试

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,8 +2,5 @@
 module.exports = {
   'src/**/*.{js,vue}': ['eslint --fix', 'git add'],
   'src/**/*.{vue,html,css,less,sass}': ['stylelint --fix', 'git add'],
-  'src/**/*.{js,vue,html,css,less,sass}': [
-    'eslint --fix --ext .js',
-    'git add'
-  ]
+  'src/**/*.{js,vue,html,css,less,sass}': ['prettier-eslint --write', 'git add']
 }
